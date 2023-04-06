e0a2e1f3bffb35ca6b5d4d569be564ff48dd531f
TanyaEdyna <114671539+TanyaEdyna@users.noreply.github.com>
2023-04-06 08:39:12 +0000

    Deploying to gh-pages from @ TanyaEdyna/goit-js-hw-11@25dae129ea969f58418c37cf67a4009f5d54e0e8 🚀

---
+++ index.7d18e217.js
@@ -0,0 +1,2 @@
+BASE_URL="https://pixabay.com/api",API_KEY="35093181-a8049340061a9729261476a01";
+//# sourceMappingURL=index.7d18e217.js.map
---
+++ index.be07e655.js
@@ -0,0 +1,2 @@
+BASE_URL="https://pixabay.com/api",API_KEY="35093181-a8049340061a9729261476a01";
+//# sourceMappingURL=index.be07e655.js.map
